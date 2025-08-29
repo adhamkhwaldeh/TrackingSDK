7c6fd345ccffc45ae5d920751a738f47d12c3c9f
adhamkhwaldeh <adhamkhwaldeh@gmail.com>
2025-08-29 19:53:08 +0200

    Refactor: Update bottom navigation bar icons
    The icons for the "List" and "Map" items in the bottom navigation bar have been updated.
    
    - "List" now uses `Icons.AutoMirrored.Filled.List`.
    - "Map" now uses `Icons.Filled.Place`.

--- app/src/main/java/com/kerberos/trackingSdk/ui/BottomNavigationBar.kt
+++ app/src/main/java/com/kerberos/trackingSdk/ui/BottomNavigationBar.kt
@@ -1,8 +1,9 @@
 package com.kerberos.trackingSdk.ui
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -19,12 +20,11 @@ import androidx.navigation.NavController
 fun BottomNavigationBar(navController: NavController) {
     var selectedItem by remember { mutableStateOf(0) }
     val items = listOf("List", "Map", "Settings")
-    val icons = listOf(Icons.Filled.List, Icons.Filled.Map, Icons.Filled.Settings)
+    val icons = listOf(Icons.AutoMirrored.Filled.List, Icons.Filled.Place, Icons.Filled.Settings)
 
     NavigationBar {
         items.forEachIndexed { index, item ->
-            NavigationBarItem(
-                icon = { Icon(icons[index], contentDescription = item) },
+            NavigationBarItem(icon = { Icon(icons[index], contentDescription = item) },
                 label = { Text(item) },
                 selected = selectedItem == index,
                 onClick = {
@@ -42,8 +42,7 @@ fun BottomNavigationBar(navController: NavController) {
                         // Restore state when reselecting a previously selected item
                         restoreState = true
                     }
-                }
-            )
+                })
         }
     }
 }
